33554aff96c73404f6b41916aef05be9594399c8
Jayasuryaaraja <jayasuryaaraja@gmail.com>
2024-08-27 14:53:29 +0530

    Add files via upload
    backend updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/",(req,res)=>{
     res.send("hello sachi");
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://jayasuryaaraja:surya1133@cluster0.nswhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,8 +26,10 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const PrductCollection = client.db("kfcDB").collection("internationalburger");
+    const PrductCollection = client.db("kfcDB").collection("Internationalburger");
+    const dprdColl = client.db("kfcDB").collection("deals");
 
+    //menu
     app.post("/product",async (req,res) =>{
         const data = req.body;
         const result = await PrductCollection.insertOne(data);
@@ -41,17 +43,17 @@ async function run() {
     });
     app.get("/product/:id",async(req,res) =>{
         const id = req.params.id;
-        const filter = {_id: new Object(id) };
+        const filter = {_id: new ObjectId(id) };
         const result = await PrductCollection.findOne(filter);
         res.send(result);
     });
     app.patch("/product/:id",async (req,res)=> {
         const id = req.params.id;
         const update  = req.body;
-        const filter = {_id: new Object(id) };
+        const filter = {_id: new ObjectId(id) };
         const updateDoc = {$set: { ...update } };
         const option = {upsert: true };
-        const result = await PrductCollection.updateOneOne(
+        const result = await PrductCollection.updateOne(
           filter,
           updateDoc,
           option
@@ -60,12 +62,54 @@ async function run() {
     });
     app.delete("/product/:id",async (req,res) =>{
         const id = req.params.id;
-        const filter = {_id: new Object(id) };
+        const filter = {_id: new ObjectId(id) };
         const result = await PrductCollection.deleteOne(filter);
         res 
         .status(200)
         .json({ success: true,message: "data deleted sucessfully",result });
     });
+
+    //deals
+    app.post("/dproduct",async (req,res) =>{
+      const data = req.body;
+      const result = await dprdColl.insertOne(data);
+      res.send(result);
+  });
+
+  app.get("/alldproduct",async (req,res) => {
+      const deals = dprdColl.find();
+      const result = await deals.toArray();
+      res.send(result);
+  });
+  app.get("/dproduct/:id",async(req,res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id) };
+      const result = await dprdColl.findOne(filter);
+      res.send(result);
+  });
+  app.patch("/dproduct/:id",async (req,res)=> {
+      const id = req.params.id;
+      const update  = req.body;
+      const filter = {_id: new ObjectId(id) };
+      const updateDoc = {$set: { ...update } };
+      const option = {upsert: true };
+      const result = await dprdColl.updateOne(
+        filter,
+        updateDoc,
+        option
+  );
+      res.send(result);
+  });
+  app.delete("/dproduct/:id",async (req,res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id) };
+      const result = await dprdColl.deleteOne(filter);
+      res 
+      .status(200)
+      .json({ success: true,message: "data deleted sucessfully",result });
+  });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
